5b2eac52de6f9260b34ec8013781fc0b584da876
Yee Poh <yee.poh@vonage.com>
2019-11-05 14:39:28 +0800

    add test for authenticator

---
+++ src/tests/authenticator.test.js
@@ -0,0 +1,115 @@
+import assert from 'assert';
+import MockContainer from './mockContainer';
+import Authenticator from '../services/authenticator';
+
+describe('Authenticator', () => {
+  it('should not authenticate properly (Missing Header)', async () => {
+    let rejected = false;
+    try {
+      const container = MockContainer();
+      const authenticator = Authenticator(container);
+
+      const req = {};
+
+      await authenticator.checkAuthentication(req, {}, (err) => {
+        if (err) {
+          rejected = true;
+          return Promise.resolve();
+        }
+        return Promise.resolve();
+      });
+    } catch (error) {
+      rejected = true;
+    }
+
+    assert.equal(rejected, true);
+  });
+
+  it('should not authenticate properly (Missing Header Value)', async () => {
+    let rejected = false;
+    try {
+      const container = MockContainer();
+      const authenticator = Authenticator(container);
+
+      const req = {
+        headers: {},
+      };
+
+      await authenticator.checkAuthentication(req, {}, (err) => {
+        if (err) {
+          rejected = true;
+          return Promise.resolve();
+        }
+        return Promise.resolve();
+      });
+    } catch (error) {
+      rejected = true;
+    }
+
+    assert.equal(rejected, true);
+  });
+
+  it('should authenticate properly (Valid auth)', async () => {
+    let rejected = false;
+    try {
+      const container = MockContainer();
+      container.authService = {
+        authenticate: async () => Promise.resolve(),
+      };
+      container.base64Service = {
+        decode: async () => Promise.resolve('abc:def'),
+      };
+      const authenticator = Authenticator(container);
+
+      const req = {
+        headers: {
+          Authorization: 'Basic aaa',
+        },
+      };
+
+      await authenticator.checkAuthentication(req, {}, (err) => {
+        if (err) {
+          rejected = true;
+          return Promise.resolve();
+        }
+        return Promise.resolve();
+      });
+    } catch (error) {
+      rejected = true;
+    }
+
+    assert.equal(rejected, false);
+  });
+
+  it('should not authenticate properly (Invalid auth scheme)', async () => {
+    let rejected = false;
+    try {
+      const container = MockContainer();
+      container.authService = {
+        authenticate: async () => Promise.resolve(),
+      };
+      container.base64Service = {
+        decode: async () => Promise.resolve('abc:def'),
+      };
+      const authenticator = Authenticator(container);
+
+      const req = {
+        headers: {
+          Authorization: 'Basics aaa',
+        },
+      };
+
+      await authenticator.checkAuthentication(req, {}, (err) => {
+        if (err) {
+          rejected = true;
+          return Promise.resolve();
+        }
+        return Promise.resolve();
+      });
+    } catch (error) {
+      rejected = true;
+    }
+
+    assert.equal(rejected, true);
+  });
+});
