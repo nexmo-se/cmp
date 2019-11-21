6bdae344a1fb1b2abc9564d16b6e5db4b7b755a7
Yee Poh <yee.poh@vonage.com>
2019-11-21 09:48:24 +0800

    add template support for sms

--- src/services/nexmo/sms.js
+++ src/services/nexmo/sms.js
@@ -1,4 +1,40 @@
 export default (container) => {
+  const getContentBody = (templateBody, parameters) => {
+    let contentBody = templateBody;
+
+    for (let i = 0; i < parameters.length; i += 1) {
+      const parameter = parameters[i];
+      const pattern = `\\{\\{${i + 1}\\}\\}`;
+      const regexp = new RegExp(pattern, 'g');
+      contentBody = contentBody.replace(regexp, parameter);
+    }
+
+    return contentBody;
+  };
+
+  const sendTemplate = async (to, templateBody, parameters, type, senderId, apiKey, apiSecret) => {
+    try {
+      const { restHost } = container.config.nexmo;
+      const text = getContentBody(templateBody, parameters);
+
+      const url = `${restHost}/sms/json`;
+      const body = {
+        api_key: apiKey,
+        api_secret: apiSecret,
+        from: senderId,
+        to,
+        text,
+        type,
+      };
+
+      const response = await container.axios.post(url, body);
+      const { data } = response;
+      return Promise.resolve(data);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
+  
   const sendSms = async (to, text, type, senderId, apiKey, apiSecret) => {
     try {
       const { restHost } = container.config.nexmo;
@@ -23,5 +59,6 @@ export default (container) => {
 
   return {
     sendSms,
+    sendTemplate,
   };
 };
