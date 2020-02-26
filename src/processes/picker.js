132a79c7cb89c57f77e025b442780cc3ae7814d7
Yee Poh <yee.poh@vonage.com>
2020-02-26 17:42:52 +0800

    add file picker support

--- src/processes/picker.js
+++ src/processes/picker.js
@@ -3,15 +3,195 @@ export default (container) => {
 
   const wait = duration => new Promise(resolve => setTimeout(resolve, duration));
 
+  const mapRecord = (csvRecord) => {
+    const record = {
+      recipient: csvRecord[0],
+      cmpParameters: [],
+    };
+
+    for (let i = 1; i < csvRecord.length; i += 1) {
+      const parameter = csvRecord[i];
+      if (parameter && parameter !== '') {
+        record.cmpParameters.push(parameter);
+      }
+    }
+
+    return record;
+  };
+
+  const createParameter = async (cmpRecordId, parameter, order) => {
+    try {
+      const { CmpParameter } = container.persistenceService;
+      const cmpParameter = await CmpParameter.createParameter(
+        cmpRecordId, parameter, order,
+      );
+      return Promise.resolve(cmpParameter);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
+
+  const createParameters = async (cmpRecordId, parameters) => {
+    try {
+      const promises = parameters.map(
+        (parameter, index) => createParameter(cmpRecordId, parameter, index),
+      );
+      const results = await Promise.all(promises);
+      return Promise.resolve(results);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
+
+  const deleteParameters = async (cmpRecordId) => {
+    try {
+      const { CmpParameter } = container.persistenceService;
+      const criteria = {
+        cmpRecordId,
+      };
+      const results = await CmpParameter.deleteParameters(criteria);
+      return Promise.resolve(results);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
+
+  const createMedia = async (media) => {
+    try {
+      const {
+        type, text, url, caption, fileName, latitude, longitude, name, address, actionUrl,
+      } = media;
+      const { CmpMedia } = container.persistenceService;
+
+      const cmpMedia = await CmpMedia.createMedia(
+        type, text, url, caption, fileName, latitude, longitude, name, address, actionUrl,
+      );
+      return Promise.resolve(cmpMedia);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
+
+  const createRecord = async (record) => {
+    try {
+      const {
+        recipient,
+        cmpCampaignId,
+        cmpTemplateId,
+        cmpMediaId,
+        cmpMedia,
+        cmpParameters,
+        activeStartHour,
+        activeStartMinute,
+        activeEndHour,
+        activeEndMinute,
+        activeOnWeekends,
+        timezone,
+      } = record;
+      const { CmpRecord } = container.persistenceService;
+
+      let actualCmpMediaId = cmpMediaId;
+      if (actualCmpMediaId == null && cmpMedia) {
+        L.debug('Creating New Media');
+        const actualCmpMedia = await createMedia(cmpMedia);
+        actualCmpMediaId = actualCmpMedia.id;
+      }
+
+
+      const sanitizedStart = container.dateTimeService
+        .getDateInUtc(activeStartHour, activeStartMinute, timezone);
+      const sanitizedEnd = container.dateTimeService
+        .getDateInUtc(activeEndHour, activeEndMinute, timezone);
+
+      const cmpRecord = await CmpRecord.createRecord(
+        recipient,
+        cmpCampaignId,
+        cmpTemplateId,
+        actualCmpMediaId,
+        sanitizedStart.getUTCHours(),
+        sanitizedStart.getUTCMinutes(),
+        sanitizedEnd.getUTCHours(),
+        sanitizedEnd.getUTCMinutes(),
+        activeOnWeekends,
+        container.dateTimeService.tzUTC,
+      );
+
+      if (cmpParameters) {
+        // Delete Previous Parameters
+        L.debug('Removing Previous Parameters');
+        await deleteParameters(cmpRecord.id);
+
+        // Create New Parameters
+        L.debug('Creating New Parameters');
+        await createParameters(cmpRecord.id, cmpParameters);
+      }
+
+
+      L.debug('Setting Record to Pending');
+      const changes = {
+        status: 'pending',
+        statusTime: new Date(),
+      };
+      const updatedCmpRecord = await CmpRecord.updateRecord(cmpRecord.id, changes, true);
+      return Promise.resolve(updatedCmpRecord);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
+
+  const runInSeqeunce = async (promises, i = 0, previousResults = []) => {
+    try {
+      if (i >= promises.length) {
+        return Promise.resolve(previousResults);
+      }
+
+      console.log(`Creating record ${i + 1}`);
+      const promise = promises[i];
+      const currentResult = await promise();
+      previousResults.push(currentResult);
+
+      const finalResults = await runInSeqeunce(promises, i + 1, previousResults);
+      return Promise.resolve(finalResults);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
+
   const processFileContent = async (jsonContent, cmpCampaignId, cmpTemplateId) => {
     try {
       const processStart = new Date().getTime();
-      L.debug(jsonContent);
       L.debug(`Campaign ID: ${cmpCampaignId}`);
       L.debug(`Template ID: ${cmpTemplateId}`);
+      const { CmpCampaign } = container.persistenceService;
+      const campaign = await CmpCampaign.readCampaign(cmpCampaignId);
+      const {
+        activeStartHour, activeStartMinute,
+        activeEndHour, activeEndMinute,
+        activeOnWeekends, timezone,
+      } = campaign;
+      const promises = jsonContent
+        .filter((_, index) => index >= 3)
+        .map(mapRecord)
+        .map(record => ({
+          recipient: record.recipient,
+          cmpCampaignId,
+          cmpTemplateId,
+          cmpMediaId: null,
+          cmpMedia: null,
+          cmpParameters: record.cmpParameters,
+          activeStartHour,
+          activeStartMinute,
+          activeEndHour,
+          activeEndMinute,
+          activeOnWeekends,
+          timezone,
+        }))
+        .map(record => createRecord(record));
+      const results = await runInSeqeunce(promises, promises.length, []);
+      L.debug(results);
       const processEnd = new Date().getTime();
       L.debug(`Time Taken (Process File Content): ${processEnd - processStart}ms`);
-      return Promise.resolve();
+      return Promise.resolve(results);
     } catch (error) {
       return Promise.reject(error);
     }
@@ -65,14 +245,19 @@ export default (container) => {
   };
 
   const archiveFile = async (fileName) => {
-    const { uploadPath, archivePath } = container.config.csv;
-    const oldPath = `${uploadPath}/${fileName}`;
-    const newPath = `${archivePath}/${fileName}`;
+    try {
+      const { uploadPath, archivePath } = container.config.csv;
+      const oldPath = `${uploadPath}/${fileName}`;
+      const newPath = `${archivePath}/${fileName}`;
 
-    const archiveStart = new Date().getTime();
-    container.fs.renameSync(oldPath, newPath);
-    const archiveEnd = new Date().getTime();
-    L.debug(`Time Taken (Archive): ${archiveEnd - archiveStart}ms`);
+      const archiveStart = new Date().getTime();
+      container.fs.renameSync(oldPath, newPath);
+      const archiveEnd = new Date().getTime();
+      L.debug(`Time Taken (Archive): ${archiveEnd - archiveStart}ms`);
+      return Promise.resolve();
+    } catch (error) {
+      return Promise.reject(error);
+    }
   };
 
   const runSingle = async () => {
@@ -88,13 +273,14 @@ export default (container) => {
           await processFile(csvFile);
 
           // Archive
-          archiveFile(csvFile);
+          await archiveFile(csvFile);
           return Promise.resolve();
         } catch (error) {
           return Promise.reject(error);
         }
       });
       const result = await Promise.all(promises);
+      L.debug(`${result.length} files processed`);
       return Promise.resolve(result);
     } catch (error) {
       return Promise.reject(error);
