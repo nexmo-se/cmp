7ff3d7bb9f8ef8b1dbfed49cbab29ac8b9a15547
Yee Poh <yee.poh@vonage.com>
2020-03-09 16:05:59 +0800

    add primary key change migration

---
+++ src/services/database/sql/migrations/20200309154930-primarykey.js
@@ -0,0 +1,42 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    const dropCmpRecordsPrimaryKey = () => queryInterface.removeConstraint('CmpRecords', 'PRIMARY');
+    const createCmpRecordsPrimaryIdColumn = () => queryInterface.addColumn('CmpRecords', 'primaryId', {
+      type: Sequelize.BIGINT(),
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    });
+
+    const dropCmpParametersPrimaryKey = () => queryInterface.removeConstraint('CmpParameters', 'PRIMARY');
+    const createCmpParametersPrimaryIdColumn = () => queryInterface.addColumn('CmpParameters', 'primaryId', {
+      type: Sequelize.BIGINT(),
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    });
+
+    return Promise.resolve()
+      .then(dropCmpRecordsPrimaryKey)
+      .then(createCmpRecordsPrimaryIdColumn)
+      .then(dropCmpParametersPrimaryKey)
+      .then(createCmpParametersPrimaryIdColumn);
+  },
+  down: (queryInterface) => {
+    const dropCmpRecordsPrimaryKey = () => queryInterface.removeConstraint('CmpRecords', 'PRIMARY');
+    const removeCmpRecordsPrimaryIdColumn = () => queryInterface.removeColumn('CmpRecords', 'primaryId');
+    const createCmpRecordsPrimaryKey = () => queryInterface.addConstraint('CmpRecords', ['id'], { type: 'primary key', name: 'PRIMARY' });
+
+    const dropCmpParametersPrimaryKey = () => queryInterface.removeConstraint('CmpParameters', 'PRIMARY');
+    const removeCmpParametersPrimaryIdColumn = () => queryInterface.removeColumn('CmpParameters', 'primaryId');
+    const createCmpParametersPrimaryKey = () => queryInterface.addConstraint('CmpCmpParametersRecords', ['id'], { type: 'primary key', name: 'PRIMARY' });
+
+    return Promise.resolve()
+      .then(dropCmpRecordsPrimaryKey)
+      .then(removeCmpRecordsPrimaryIdColumn)
+      .then(createCmpRecordsPrimaryKey)
+      .then(dropCmpParametersPrimaryKey)
+      .then(removeCmpParametersPrimaryIdColumn)
+      .then(createCmpParametersPrimaryKey);
+  },
+};
