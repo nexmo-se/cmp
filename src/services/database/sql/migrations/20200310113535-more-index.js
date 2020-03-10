d214950eb9cb1552ee2c260067e7bf74955aee1a
Yee Poh <yee.poh@vonage.com>
2020-03-10 11:43:46 +0800

    add more index to facilitate blasting

---
+++ src/services/database/sql/migrations/20200310113535-more-index.js
@@ -0,0 +1,46 @@
+module.exports = {
+  up: (queryInterface) => {
+    const addCmpRecordsCmpCampaignIdStatusDeleted = () => queryInterface.addIndex(
+      'CmpRecords',
+      [
+        'cmpCampaignId',
+        'status',
+        'deleted',
+      ],
+    );
+    const addCmpCampaignsIdDeleted = () => queryInterface.addIndex(
+      'CmpCampaigns',
+      [
+        'id',
+        'deleted',
+      ],
+    );
+    // create index cmpCampaignId_status_deleted on CmpRecords (cmpCampaignId, status, deleted);
+
+    // create index id_deleted on CmpCampaigns (id, deleted);
+    return Promise.resolve()
+      .then(addCmpRecordsCmpCampaignIdStatusDeleted)
+      .then(addCmpCampaignsIdDeleted);
+  },
+  down: (queryInterface) => {
+    const removeCmpRecordsCmpCampaignIdStatusDeleted = () => queryInterface.removeIndex(
+      'CmpRecCmpRecordsords',
+      [
+        'cmpCampaignId',
+        'status',
+        'deleted',
+      ],
+    );
+    const removeCmpCampaignsIdDeleted = () => queryInterface.removeIndex(
+      'CmpCampaigns',
+      [
+        'id',
+        'deleted',
+      ],
+    );
+
+    return Promise.resolve()
+      .then(removeCmpRecordsCmpCampaignIdStatusDeleted)
+      .then(removeCmpCampaignsIdDeleted);
+  },
+};
