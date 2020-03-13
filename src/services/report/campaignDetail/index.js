1cc39a8fdb713d44044ac0913fdc9f44cafcba57
Yee Poh <yee.poh@vonage.com>
2020-03-13 15:27:09 +0800

    add support for generating campaign detail report

---
+++ src/services/report/campaignDetail/index.js
@@ -0,0 +1,63 @@
+export default (container) => {
+  const { L } = container.defaultLogger('Report Service - Campaign Detail');
+
+  const isUuid = (uuid) => {
+    const uuidRegex = '^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$';
+    const result = new RegExp(uuidRegex).test(uuid);
+    return result;
+  };
+
+  const mapCmpRecord = (cmpRecord) => {
+    const mappedCmpRecord = {
+      id: cmpRecord.id,
+      recipient: cmpRecord.recipient,
+      cmpChannel: {
+        id: cmpRecord.cmpTemplate.cmpChannel.id,
+        recipient: cmpRecord.cmpTemplate.cmpChannel.name,
+        channel: cmpRecord.cmpTemplate.cmpChannel.channel,
+      },
+      cmpTemplate: {
+        id: cmpRecord.cmpTemplate.id,
+        name: cmpRecord.cmpTemplate.name,
+      },
+    };
+
+    if (cmpRecord.cmpRecordMessages && cmpRecord.cmpRecordMessages.length > 0) {
+      const cmpRecordMessage = cmpRecord.cmpRecordMessages[0];
+      mappedCmpRecord.status = cmpRecordMessage.status;
+      mappedCmpRecord.statusTime = cmpRecordMessage.statusTime;
+    } else {
+      mappedCmpRecord.status = cmpRecord.status;
+      mappedCmpRecord.statusTime = cmpRecord.statusTime;
+    }
+    mappedCmpRecord.submitTime = cmpRecord.sendTime;
+    return mappedCmpRecord;
+  };
+
+  const getCampaignDetails = async (cmpCampaignId, limit = 30, offset = 0) => {
+    try {
+      const isValid = isUuid(cmpCampaignId);
+      if (!isValid) {
+        return Promise.reject(new container.BadRequestError('Invalid cmpCampaignId, expected a proper UUID for Campaign ID'));
+      }
+
+      const { CmpRecord } = container.persistenceService;
+      const criteria = { cmpCampaignId };
+      const cmpRecords = await CmpRecord.findRecords(criteria, true, { limit, offset });
+
+      const report = {
+        results: cmpRecords.map(mapCmpRecord),
+        limit,
+        offset,
+      };
+
+      return Promise.resolve(report);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
+
+  return {
+    getCampaignDetails,
+  };
+};
--- src/services/report/index.js
+++ src/services/report/index.js
@@ -1,9 +1,11 @@
 import Summary from './summary';
+import CampaignDetail from './campaignDetail';
 
 export default (container) => {
   const { L } = container.defaultLogger('Report Service');
 
   return {
     summary: Summary(container),
+    campaignDetail: CampaignDetail(container),
   };
 };
