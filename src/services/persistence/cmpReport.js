2bdfff48e0d9aa0b1a29c9aa0cfb2c644776fc4c
Yee Poh <yee.poh@vonage.com>
2020-03-13 16:54:43 +0800

    add subreport logic for persistence service

--- src/services/persistence/cmpReport.js
+++ src/services/persistence/cmpReport.js
@@ -1,11 +1,26 @@
 export default (container) => {
   const { L } = container.defaultLogger('Cmp Report Persistence Accessor');
 
+  const mapReport = (report) => {
+    const reportData = report;
+
+    if (report.type === 'overall_summary') {
+      reportData.content = report.cmpReportOverallSummary;
+    } else if (report.type === 'campaign_summary') {
+      reportData.content = report.cmpReportCampaignSummary;
+    } else if (report.type === 'campaign_detail') {
+      reportData.content = report.cmpReportCampaignDetail;
+    }
+
+    return reportData;
+  };
+
   const listReports = async (options = {}) => {
     try {
       const { CmpReport } = container.databaseService.accessors;
       const cmpReports = await CmpReport.listReports(options);
-      return Promise.resolve(cmpReports);
+      const mappedCmpReports = cmpReports.map(mapReport);
+      return Promise.resolve(mappedCmpReports);
     } catch (error) {
       return Promise.reject(error);
     }
@@ -15,7 +30,8 @@ export default (container) => {
     try {
       const { CmpReport } = container.databaseService.accessors;
       const cmpReport = await CmpReport.findReport(criteria, true);
-      return Promise.resolve(cmpReport);
+      const mappedCmpReport = mapReport(cmpReport);
+      return Promise.resolve(mappedCmpReport);
     } catch (error) {
       return Promise.reject(error);
     }
@@ -25,23 +41,89 @@ export default (container) => {
     try {
       const { CmpReport } = container.databaseService.accessors;
       const cmpReports = await CmpReport.findReports(criteria, true, options);
-      return Promise.resolve(cmpReports);
+      const mappedCmpReports = cmpReports.map(mapReport);
+      return Promise.resolve(mappedCmpReports);
     } catch (error) {
       return Promise.reject(error);
     }
   };
 
-  const createReport = async (
-    type,
-    name,
+  const createOverallSummary = async (
+    type, name, content,
   ) => {
     try {
-      const { CmpReport } = container.databaseService.accessors;
+      const { CmpReport, CmpReportOverallSummary } = container.databaseService.accessors;
+
+      // Create Media Text
+      const { from, to } = content;
+      const cmpReportOverallSummary = await CmpReportOverallSummary
+        .createReportOverallSummary(from, to);
+      const cmpReportOverallSummaryId = cmpReportOverallSummary.id;
+
+      // Create Media
       const cmpReport = await CmpReport.createReport(
         type,
         name,
+        cmpReportOverallSummaryId,
+        null,
+        null,
       );
-      return Promise.resolve(cmpReport);
+      const mappedReport = mapReport(cmpReport);
+      return Promise.resolve(mappedReport);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
+
+  const createCampaignSummary = async (
+    type, name, content,
+  ) => {
+    try {
+      const { CmpReport, CmpReportCampaignSummary } = container.databaseService.accessors;
+
+      // Create Media Text
+      const { cmpCampaignId, from, to } = content;
+      const cmpReportCampaignSummary = await CmpReportCampaignSummary
+        .createReportCampaignSummary(cmpCampaignId, from, to);
+      const cmpReportCampaignSummaryId = cmpReportCampaignSummary.id;
+
+      // Create Media
+      const cmpReport = await CmpReport.createReport(
+        type,
+        name,
+        null,
+        cmpReportCampaignSummaryId,
+        null,
+      );
+      const mappedReport = mapReport(cmpReport);
+      return Promise.resolve(mappedReport);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
+
+  const createCampaignDetail = async (
+    type, name, content,
+  ) => {
+    try {
+      const { CmpReport, CmpReportCampaignDetail } = container.databaseService.accessors;
+
+      // Create Media Text
+      const { cmpCampaignId, from, to } = content;
+      const cmpReportCampaignDetail = await CmpReportCampaignDetail
+        .createReportCampaignDetail(cmpCampaignId, from, to);
+      const cmpReportCampaignDetailId = cmpReportCampaignDetail.id;
+
+      // Create Media
+      const cmpReport = await CmpReport.createReport(
+        type,
+        name,
+        null,
+        null,
+        cmpReportCampaignDetailId,
+      );
+      const mappedReport = mapReport(cmpReport);
+      return Promise.resolve(mappedReport);
     } catch (error) {
       return Promise.reject(error);
     }
@@ -51,7 +133,8 @@ export default (container) => {
     try {
       const { CmpReport } = container.databaseService.accessors;
       const cmpReport = await CmpReport.readReport(cmpReportId);
-      return Promise.resolve(cmpReport);
+      const mappedCmpReport = mapReport(cmpReport);
+      return Promise.resolve(mappedCmpReport);
     } catch (error) {
       return Promise.reject(error);
     }
@@ -63,7 +146,8 @@ export default (container) => {
       const cmpReport = await CmpReport.updateReport(
         cmpReportId, changes,
       );
-      return Promise.resolve(cmpReport);
+      const mappedCmpReport = mapReport(cmpReport);
+      return Promise.resolve(mappedCmpReport);
     } catch (error) {
       return Promise.reject(error);
     }
@@ -75,7 +159,8 @@ export default (container) => {
       const cmpReports = await CmpReport.updateReports(
         criteria, changes, options,
       );
-      return Promise.resolve(cmpReports);
+      const mappedCmpReports = cmpReports.map(mapReport);
+      return Promise.resolve(mappedCmpReports);
     } catch (error) {
       return Promise.reject(error);
     }
@@ -85,7 +170,8 @@ export default (container) => {
     try {
       const { CmpReport } = container.databaseService.accessors;
       const cmpReport = await CmpReport.deleteReport(cmpReportId);
-      return Promise.resolve(cmpReport);
+      const mappedCmpReport = mapReport(cmpReport);
+      return Promise.resolve(mappedCmpReport);
     } catch (error) {
       return Promise.reject(error);
     }
@@ -95,7 +181,8 @@ export default (container) => {
     try {
       const { CmpReport } = container.databaseService.accessors;
       const cmpReports = await CmpReport.deleteReports(criteria);
-      return Promise.resolve(cmpReports);
+      const mappedCmpReports = cmpReports.map(mapReport);
+      return Promise.resolve(mappedCmpReports);
     } catch (error) {
       return Promise.reject(error);
     }
@@ -105,7 +192,10 @@ export default (container) => {
   return {
     listReports,
 
-    createReport,
+    createOverallSummary,
+    createCampaignSummary,
+    createCampaignDetail,
+
     readReport,
 
     updateReport,
