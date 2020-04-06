4bd330dab13cbde85b2b7097b6313d1b2f9d85bb
Yee Poh <yee.poh@vonage.com>
2020-04-06 11:59:20 +0800

    add support for generating campaign detail report on end of campaign

---
+++ src/processes/blaster/campaignDetail.js
@@ -0,0 +1,81 @@
+export default (container) => {
+  const { L } = container.defaultLogger('Report Process - Campaign Detail');
+
+  const appendHeader = async (filePath) => {
+    try {
+      const startTime = new Date().getTime();
+
+      // Write Header
+      const header = [['id', 'recipient', 'channel', 'template', 'status', 'status time', 'submit time']];
+      const headerCsv = await container.csvService.toCsv(header);
+      await container.fileService.writeContent(filePath, headerCsv);
+
+      const endTime = new Date().getTime();
+      L.debug(`Time Taken (Append Header): ${endTime - startTime}`);
+      return Promise.resolve();
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
+
+  const appendContent = async (filePath, records) => {
+    try {
+      const startTime = new Date().getTime();
+
+      // Write Content
+      const timePattern = 'YYYY/MM/DD HH:mm:ss';
+      const content = records.map(record => ([
+        record.id,
+        record.recipient,
+        (record.cmpChannel || {}).channel,
+        (record.cmpTemplate || {}).name,
+        record.status,
+        record.statusTime ? container.moment(record.statusTime).format(timePattern) : null,
+        record.submitTime ? container.moment(record.submitTime).format(timePattern) : null,
+      ]));
+
+      const contentCsv = await container.csvService.toCsv(content);
+      await container.fileService.writeContent(filePath, contentCsv);
+
+      const endTime = new Date().getTime();
+      L.debug(`Time Taken (Append Content) [${records.length}]: ${endTime - startTime}`);
+      return Promise.resolve();
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
+
+  const generateCampaignNextBatch = async (cmpCampaignId, filePath, limit, offset) => {
+    try {
+      const { campaignDetail: campaignDetailService } = container.reportService;
+      const paginatedResults = await campaignDetailService
+        .getCampaignDetails(cmpCampaignId, limit, offset);
+
+      const { results } = paginatedResults;
+
+      await appendContent(filePath, results);
+
+      if (results.length <= 0) {
+        return Promise.resolve();
+      }
+      return generateCampaignNextBatch(cmpCampaignId, filePath, limit, offset + results.length);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
+
+  const generateCampaign = async (cmpCampaignId, filePath) => {
+    try {
+      const { batchLimit } = container.config.report;
+      await appendHeader(filePath);
+      await generateCampaignNextBatch(cmpCampaignId, filePath, batchLimit, 0);
+      return Promise.resolve();
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
+
+  return {
+    generateCampaign,
+  };
+};
--- src/processes/blaster/index.js
+++ src/processes/blaster/index.js
@@ -1,5 +1,9 @@
+import CampaignDetailReporter from './campaignDetail';
+
 export default (container) => {
   const { L } = container.defaultLogger('Blaster Process');
+
+  const campaignDetailReporter = CampaignDetailReporter(container);
 
   const getRecords = async (recordSize) => {
     try {
@@ -142,6 +146,7 @@ export default (container) => {
 
   const updateCampaignStatus = async (campaign, isStart = true, isEnd = true) => {
     try {
+      const { generateReport } = container.config.blaster;
       const { CmpRecord, CmpCampaign } = container.persistenceService;
       const cmpCampaignId = campaign.id;
       const currentTime = new Date();
@@ -166,15 +171,37 @@ export default (container) => {
         L.debug(`Time Taken (Count Pending/Queuing Records): ${duration}ms`);
 
         if (recordsCount === 0) {
-          const actualStartDate = campaign.actualStartDate || currentTime;
           // End, last record
-          changes.status = 'completed';
-          changes.statusTime = currentTime;
+          const actualStartDate = campaign.actualStartDate || currentTime;
           changes.actualEndDate = currentTime;
           changes.actualDuration = currentTime.getTime() - actualStartDate.getTime();
-          L.trace(`Updating Campaign ${cmpCampaignId} Status to completed at ${currentTime}`);
 
-          await publishCampaignStatusAudit(campaign, 'completed');
+          if (generateReport) {
+            // Reporting State
+            changes.status = 'reporting';
+            changes.statusTime = currentTime;
+            L.trace(`Updating Campaign ${cmpCampaignId} Status to reporting at ${currentTime}`);
+
+            await publishCampaignStatusAudit(campaign, 'reporting');
+
+            // Generate Report
+            const { filePath } = container.config.report;
+            const fileName = `${cmpCampaignId}.csv`;
+            const fullPath = `${filePath}/${fileName}`;
+            campaignDetailReporter.generateCampaign(cmpCampaignId, fullPath)
+              .then(() => publishCampaignStatusAudit(campaign, 'completed'))
+              .then(() => CmpCampaign.updateCampaign(cmpCampaignId, {
+                status: 'completed',
+                statusTime: new Date(),
+              }));
+          } else {
+            // Direct Complete
+            changes.status = 'completed';
+            changes.statusTime = currentTime;
+            L.trace(`Updating Campaign ${cmpCampaignId} Status to completed at ${currentTime}`);
+
+            await publishCampaignStatusAudit(campaign, 'completed');
+          }
         }
       }
 
